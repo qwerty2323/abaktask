92c3bcbaf25c0995e2abb81c508d8d1bd4784ac1
Sergey <sclinede@gmail.com>
2013-02-21 23:19:13 -0500

    Scaffold of Abak Task

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AbakTask::Application.config.secret_token = '9844b50d206ca0920fb49e346bf5afd62c55ff86a04ba9fd2aeef414a46dd8a03218f2828d6fe53f328519ef57414377a00afc29eafe9cb4793c0fd0888224df'
